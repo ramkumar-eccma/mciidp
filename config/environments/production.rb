76a66ce0e1c8dc89344c748ac87fbef3e6a94ec8
Ramkumar <ramkumar@eccma.org>
2016-01-27 14:33:41 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Myapp::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = false
+  config.serve_static_files = true
   ENKEY = "ksademo"
 
   # Compress JavaScripts and CSS.
